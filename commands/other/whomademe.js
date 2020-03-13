93ce7bae48a9f0a41d44fc7f90f75d97aaf255d6
X0r1 <62038775+X0r1@users.noreply.github.com>
2020-03-13 17:30:56 +0200

    Update whomademe.js

--- commands/other/whomademe.js
+++ commands/other/whomademe.js
@@ -13,7 +13,7 @@ module.exports = class WhoMadeMeCommand extends Command {
 
   run(message) {
     message.say(
-      'Made by @hyperzone#1185 with :heart: full code is available on GitHub https://github.com/galnir/Master-Bot'
+      'Made by @James Charles#9676 with :heart: '
     );
   }
 };
